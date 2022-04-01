44a5fb785a3e8310ef44a4dcebd7645d06675f36
Phumphathai Chansriwong <ouoam2555@gmail.com>
2022-04-01 10:33:10 +0700

    Finish Lab10

--- LAB10/behavioral/NewsPublisher.java
+++ LAB10/behavioral/NewsPublisher.java
@@ -1,8 +1,23 @@
 package edu.parinya.softarchdesign.behavioral;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 
 public class NewsPublisher implements Publisher<News> {
-	// You need to implement this class
+    // You need to implement this class
+    List<Subscriber<? super News>> subscribers = new ArrayList<>();
+
+    @Override
+    public void subscribe(Subscriber<? super News> subscriber) {
+        subscribers.add(subscriber);
+    }
+
+    public void publish(News news) {
+        for (Subscriber<? super News> subscriber : subscribers) {
+            subscriber.onNext(news);
+            subscriber.onComplete();
+        }
+    }
 }
--- LAB10/behavioral/NewsSubscriber.java
+++ LAB10/behavioral/NewsSubscriber.java
@@ -4,5 +4,30 @@ import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
 
 public class NewsSubscriber implements Subscriber<News> {
-	// You need to implement this class
+    // You need to implement this class
+    String user;
+
+    NewsSubscriber(String user) {
+        this.user = user;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+
+    }
+
+    @Override
+    public void onNext(News item) {
+        System.out.println(user + " \t" + item.getTopic() + "\t" + item.getContent());
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
 }
