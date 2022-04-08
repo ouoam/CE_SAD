33c55767f080992218376cf94f2296a559c73de2
Phumphathai Chansriwong <ouoam2555@gmail.com>
2022-04-08 10:22:29 +0700

    Finish LAB11 code
    repl.it free version is so slow

--- LAB11/behavioral2/HeightSortStrategy.java
+++ LAB11/behavioral2/HeightSortStrategy.java
@@ -5,5 +5,7 @@ import java.util.List;
 
 public class HeightSortStrategy implements SortStrategy {
     // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
-
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparing(Person::getHeight));
+    }
 }
